00b5acc02865a2440b3cbed878db33909f97c810
Lawton Ives <Lawton@Lawtons-MacBook-Pro.local>
2017-05-17 15:33:14 -0400

    Reworking controller for new layout.

--- contactApp/Controller.java
+++ contactApp/Controller.java
@@ -6,6 +6,7 @@ import java.util.ResourceBundle;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 import javafx.application.Platform;
 
 public class Controller implements Initializable {
@@ -20,19 +21,36 @@ public class Controller implements Initializable {
 		Platform.exit();
 	}
 
-	public Button submitButton; //the fxid needs to be the same as the object name.
-	public TextField nameEntry;
-	public TextField emailEntry;
-	public TextField phoneEntry;
-	public void submitButtonClick(){
-		System.out.println("Contact Data:");
-		System.out.println("Name: " + nameEntry.getText());
-		System.out.println("Email: " + emailEntry.getText());
-		System.out.println("Phone #: " + phoneEntry.getText());
-		System.out.println();
-		makeNewContact(nameEntry.getText(), emailEntry.getText(), phoneEntry.getText());
-//		submitButton.setText("Clicked!");
+	//To make new contact
+	public Button newContactButton;
+	public Label nameField;
+	public void newContactScreen(){
+		//TODO: move to new contact making screen
+		System.out.println("User wants to make a new contact.");
+		
 	}
+	
+	//Converts a label to a TextField
+	public void toTextField(Label oLabel){
+		String value = oLabel.getText();
+		System.out.println(value);
+	}
+
+
+//The following might be useful for the update screen.
+//	public Button submitButton; //the fxid needs to be the same as the object name.
+//	public TextField nameEntry;
+//	public TextField emailEntry;
+//	public TextField phoneEntry;
+//	public void submitButtonClick(){
+//		System.out.println("Contact Data:");
+//		System.out.println("Name: " + nameEntry.getText());
+//		System.out.println("Email: " + emailEntry.getText());
+//		System.out.println("Phone #: " + phoneEntry.getText());
+//		System.out.println();
+//		makeNewContact(nameEntry.getText(), emailEntry.getText(), phoneEntry.getText());
+//		submitButton.setText("Clicked!");
+//	}
 
 	//public ArrayList<Contact> contacts = new ArrayList<>();
 	public void makeNewContact(String name, String email, String phone){
--- contactApp/Main.java
+++ contactApp/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application{
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("example.fxml"));
         primaryStage.setTitle("Contact Manager");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 650, 400));
         primaryStage.show();
     }
 }
