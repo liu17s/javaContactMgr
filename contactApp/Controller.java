932a4ab7b645752781e6dfc2eb73f6d9c62b5d42
Lawton Ives <Lawton@Lawtons-MacBook-Pro.local>
2017-05-22 16:41:09 -0400

    Added ability to change Labels into TextFields in order to make a new contact.

--- contactApp/Controller.java
+++ contactApp/Controller.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.application.Platform;
 
 public class Controller implements Initializable {
@@ -17,23 +18,60 @@ public class Controller implements Initializable {
     }
 	
 	public MenuItem quitMenuItem;
+	/**
+	*	Method that quits the app using Platform.exit().
+	*/
 	public void quitApp(){
 		Platform.exit();
 	}
 
 	//To make new contact
 	public Button newContactButton;
-	public Label nameField;
+	public Button updateButton;
+	public Label nameLbl;
+	public Label addressLbl;
+	public Label emailLbl;
+	public Label phoneLbl;
+	public Pane editPane;
+	
+	/**
+	*	Used to change to editPane to something that is functional for making a new contact.
+	*/
 	public void newContactScreen(){
 		//TODO: move to new contact making screen
 		System.out.println("User wants to make a new contact.");
 		
+		//Adds new TextFields to visible Pane for edits
+		editPane.getChildren().addAll(toTextField(nameLbl), toTextField(addressLbl), toTextField(emailLbl), toTextField(phoneLbl));
+		nameLbl.setVisible(false);
+		addressLbl.setVisible(false);
+		emailLbl.setVisible(false);
+		phoneLbl.setVisible(false);
 	}
 	
-	//Converts a label to a TextField
-	public void toTextField(Label oLabel){
+	/**
+	*	Converts a label to a TextField.
+	*	@param oLabel is of type Label
+	*	@returns a new TextField with the same value and position as oLabel with a similar id (except with "Filed" in place of "Lbl" at the end).
+	*/
+	public TextField toTextField(Label oLabel){
+		
+		//Takes value and position from original label
 		String value = oLabel.getText();
-		System.out.println(value);
+		String id = oLabel.getId();
+		double x = oLabel.getLayoutX();
+		double y = oLabel.getLayoutY();
+		
+		//New id has same beginning but replaces "Lbl" with "Field"
+		id = id.substring(0, id.length() - 3) + "Field";
+		
+		//Creates a new TextField with the same position and value as oLabel
+		TextField nTF = new TextField(value);
+		nTF.setId(id);
+		nTF.setLayoutX(x);
+		nTF.setLayoutY(y - 5); //5 is subtracted to correct an appearance problem.
+		System.out.println(nTF.getId());
+		return nTF;
 	}
 
 
