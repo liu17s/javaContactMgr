5d2e1f9716ed1f52f4d31c1d762c6ecfc1e659b0
Lawton Ives <Lawton@Lawtons-MacBook-Pro.local>
2017-05-23 14:52:11 -0400

    Added id field to be able to differntiate between contacts.

--- contactApp/Contact.java
+++ contactApp/Contact.java
@@ -3,12 +3,14 @@ package contactApp;
 import java.io.Serializable;
 
 public class Contact implements Serializable{
+	public int id;
 	public String name;
 	public String address;
 	public String email;
 	public String phone;
 	
-	public Contact(String name, String address, String email, String phone){
+	public Contact(int id, String name, String address, String email, String phone){
+		this.id = id;
 		this.name = name;
 		this.address = address;
 		this.email = email;
